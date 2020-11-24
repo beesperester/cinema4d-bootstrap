098a936ee1f5f43a541d4de232f0b20c24891a96
Bernhard Esperester <bernhard@esperester.de>
2020-11-24 14:05:10 +0100

    adds tests for utilities module

---
+++ tests/utilities/cli_test.py
@@ -0,0 +1,22 @@
+"""Test utilities cli module."""
+
+import unittest
+
+from bootstrap.utilities.cli import cli_format_error, cli_format_success
+
+
+class TestCliMethods(unittest.TestCase):
+
+    def test_cli_format_error(self):
+        message = "Hello World"
+        result = cli_format_error(message)
+        excpected_result = "\x1b[31mError:\x1b[0m {}".format(message)
+
+        self.assertEqual(result, excpected_result)
+
+    def test_cli_format_success(self):
+        message = "Hello World"
+        result = cli_format_success(message)
+        excpected_result = "\x1b[32mSuccess:\x1b[0m {}".format(message)
+
+        self.assertEqual(result, excpected_result)
